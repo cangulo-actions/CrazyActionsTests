084a9ab830206afc62a28b495c54cce6662c6244
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-12-11 02:35:38 +0100

    Add CYPRESS_GH_API_URL environment variable

--- run-cypress.sh
+++ run-cypress.sh
@@ -1,5 +1,6 @@
 export CYPRESS_OWNER="cangulo-actions"
 export CYPRESS_REPO="CrazyActionsTests"
 export CYPRESS_SEMVER_PR_NUMBER="LOCAl-TEST"
+export CYPRESS_GH_API_URL="https://api.github.com/repos/$CYPRESS_OWNER/$CYPRESS_REPO"
 
 npx cypress open
