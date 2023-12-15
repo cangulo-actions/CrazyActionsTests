2c567f46856ded2bc0441960a0a3444e24e8d408
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-12-15 11:31:54 +0100

    refactor: rename scripts and moved to scripts folder

---
+++ scripts/0-import-cypress-tests.sh
@@ -0,0 +1,4 @@
+echo "Importing Cypress tests from cangulo-actions/semver"
+
+rm -rf ./cypress
+cp -r ../semver/cypress/* ./
--- scripts/1-run-cypress.sh
+++ scripts/1-run-cypress.sh
@@ -1,6 +1,6 @@
 export CYPRESS_OWNER="cangulo-actions"
 export CYPRESS_REPO="CrazyActionsTests"
-export CYPRESS_SEMVER_PR_NUMBER="LOCAl-TEST"
 export CYPRESS_GH_API_URL="https://api.github.com/repos/$CYPRESS_OWNER/$CYPRESS_REPO"
+export CYPRESS_SEMVER_PR_NUMBER="LOCAl-TEST"
 
-npx cypress open
+npx cypress run -e2e --headless --no-runner-ui 'cypress/e2e/smoke-test.feature'
