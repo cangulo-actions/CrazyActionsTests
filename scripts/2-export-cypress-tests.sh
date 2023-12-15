3479f753a33fc49cc6ccd8923939996f71966652
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-12-15 18:23:40 +0100

    fix: improved scripts to run tests by tag

--- scripts/2-export-cypress-tests.sh
+++ scripts/2-export-cypress-tests.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+
 cp -r ./cypress/* ../semver/cypress/cypress/
+
+files=('cypress.config.js' 'package.json' 'package-lock.json')
+
+for file in "${files[@]}"; do
+    cp ./$file ../semver/cypress/
+done
