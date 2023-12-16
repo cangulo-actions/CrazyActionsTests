9f84c3859d492d1d0c3952df8d1a5b58235c07ee
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-12-16 13:24:42 +0100

    fix: improved scripts to run tests by tag

--- scripts/1-run-cypress.sh
+++ scripts/1-run-cypress.sh
@@ -6,6 +6,7 @@ export CYPRESS_GH_TOKEN=$(gh auth token)
 export CYPRESS_OWNER="cangulo-actions"
 export CYPRESS_REPO="CrazyActionsTests"
 export CYPRESS_GH_API_URL="https://api.github.com/repos/$CYPRESS_OWNER/$CYPRESS_REPO"
+export CYPRESS_SEMVER_BRANCH="#61"
 export CYPRESS_SEMVER_PR_NUMBER="LOCAl-TEST"
 # export CYPRESS_TAGS="@smoke"
 # export CYPRESS_TAGS="not @smoke" # run all tests
