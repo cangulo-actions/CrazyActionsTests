9b72b36eeed66d135b58e4c526ad4c1d19dbde5f
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-12-17 20:40:41 +0100

    refactor: rename job step

--- scripts/1-run-cypress.sh
+++ scripts/1-run-cypress.sh
@@ -6,16 +6,24 @@ export CYPRESS_GH_TOKEN=$(gh auth token)
 export CYPRESS_OWNER="cangulo-actions"
 export CYPRESS_REPO="CrazyActionsTests"
 export CYPRESS_GH_API_URL="https://api.github.com/repos/$CYPRESS_OWNER/$CYPRESS_REPO"
-export CYPRESS_SEMVER_BRANCH="#61"
+export CYPRESS_SEMVER_BRANCH="main"
 export CYPRESS_SEMVER_PR_NUMBER="LOCAl-TEST"
-# export CYPRESS_TAGS="@smoke"
+
+export CYPRESS_BEFORE_ALL_RESET_REPO_ENABLED="true"
+export CYPRESS_AFTER_ALL_RESET_REPO_ENABLED="false"
+export CYPRESS_GH_WORKFLOW_RESET_REPO_TIMEOUT="30000"
+
+export CYPRESS_WAIT_TIME_CD_WORKFLOW="25000"
+export CYPRESS_API_RETRY_INTERVAL_MS="10000"
+export CYPRESS_API_RETRY_TIMEOUT_MS="40000"
+export CYPRESS_TAGS="@smoke"
 # export CYPRESS_TAGS="not @smoke" # run all tests
 
 smoke_spec="cypress/e2e/smoke-test.feature"
 
-# cypress run --e2e --headless --no-runner-ui --spec $smoke_spec
+cypress run --e2e --headless --no-runner-ui --spec $smoke_spec
 # cypress run --e2e --headless --no-runner-ui
-cypress open --e2e
+# cypress open --e2e
 
 git checkout main
 git pull origin main
