1dcfdc5ce4949d85411d5ef0c7285530aea81555
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-12-17 23:30:11 +0100

    fix: improved scripts to run tests by tag

--- scripts/1-run-cypress.sh
+++ scripts/1-run-cypress.sh
@@ -2,14 +2,16 @@ git fetch
 git checkout main
 git pull origin main
 
+owner="cangulo-actions"
+repo="CrazyActionsTests"
+
 export CYPRESS_GH_TOKEN=$(gh auth token)
-export CYPRESS_OWNER="cangulo-actions"
-export CYPRESS_REPO="CrazyActionsTests"
-export CYPRESS_GH_API_URL="https://api.github.com/repos/$CYPRESS_OWNER/$CYPRESS_REPO"
+export CYPRESS_GH_API_URL="https://api.github.com/repos/${owner}/${repo}"
 export CYPRESS_SEMVER_BRANCH="main"
 export CYPRESS_SEMVER_PR_NUMBER="LOCAl-TEST"
+export CYPRESS_BRANCH_TO_CREATE="LOCAl-TEST"
 
-export CYPRESS_BEFORE_ALL_RESET_REPO_ENABLED="true"
+export CYPRESS_BEFORE_ALL_RESET_REPO_ENABLED="false"
 export CYPRESS_AFTER_ALL_RESET_REPO_ENABLED="false"
 export CYPRESS_GH_WORKFLOW_RESET_REPO_TIMEOUT="30000"
 
@@ -19,11 +21,14 @@ export CYPRESS_API_RETRY_TIMEOUT_MS="40000"
 export CYPRESS_TAGS="@smoke"
 # export CYPRESS_TAGS="not @smoke" # run all tests
 
-smoke_spec="cypress/e2e/smoke-test.feature"
+smoke_spec="./cypress/e2e/smoke-test.feature"
 
-cypress run --e2e --headless --no-runner-ui --spec $smoke_spec
+# cypress run --e2e --headless --no-runner-ui --spec $smoke_spec
 # cypress run --e2e --headless --no-runner-ui
-# cypress open --e2e
+cypress open --e2e \
+    --config-file cypress.config.js \
+    --project ./ \
+    --browser electron 
 
 git checkout main
 git pull origin main
